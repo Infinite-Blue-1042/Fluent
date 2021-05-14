9ccbea5abffe3c48210184796f77247d9865ec84
Aarav Dave <aarinaarav@gmail.com>
2021-05-14 19:51:20 -0400

    Update compile.py

--- compile.py
+++ compile.py
@@ -5,15 +5,15 @@
 from tkinter import *
 
 
-def error(length):
-    if len(param) != length:
+def error(length, equal=True):
+    if (len(param) != length and equal) or (len(param) < length and not equal):
         raise RuntimeError
 
 
 __author__ = 'Aarav Dave'
 __file__ = 'code.fl'  #*#
 
-variables = {}
+vars = {}
 
 with open(__file__) as file:
     for line in file:
@@ -25,18 +25,35 @@ with open(__file__) as file:
             command, param = param[0], param[1:]
             if command == 'create':
                 error(1)
-                variables[param[0]] = Tk()
-                variables[param[0]].title('app')
+                vars[param[0]] = Tk()
+                vars[param[0]].title('app')
             elif command == 'rename':
-                error(2)
-                variables[param[0]].title(' '.join(param[1:]))
+                error(2, False)
+                vars[param[0]].title(' '.join(param[1:]))
             elif command == 'size':
                 error(3)
-                variables[param[0]].geometry(f'{param[1]}x{param[2]}')
+                vars[param[0]].geometry(f'{param[1]}x{param[2]}')
             elif command == 'run':
                 error(1)
-                variables[param[0]].mainloop()
+                vars[param[0]].mainloop()
             else:
                 error(-1)
+        elif command == 'place':
+            error(1, False)
+            values = {'x': 0, 'y': 0, 'width': 50, 'height': 50}
+            for value in param[1:]:
+                value = value.split('=')
+                values[value[0]] = value[1]
+            vars[param[0]].place(x=int(values['x']), y=int(values['y']), width=int(values['width']), height=int(values['height']))
+        elif command == 'button':
+            command, param = param[0], param[1:]
+            if command == 'create':
+                error(2)
+                vars[param[1]] = Button(vars[param[0]])
+        elif command == 'entry':
+            command, param = param[0], param[1:]
+            if command == 'create':
+                error(2)
+                vars[param[1]] = Text(vars[param[0]])
         else:
             error(-1)
